2fb61ff5ec9b00c23e2c79fb81ba5bb3b64f5079
Ioannis "Giannis" Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2025-03-11 21:49:03 +0100

    Fix backgammon stakes (#39)
    * Fix backgammon stakes
    
    * Fix ruff

---
+++ test/test_winner.py
@@ -0,0 +1,16 @@
+"""Test the get_winner functionality of the Backgammon environment."""
+
+from rlgammon.environment import BackgammonEnv
+
+BACKGAMMON_STAKES = 3
+
+
+def test_get_winner() -> None:
+    """Test the get_winner method of the Backgammon environment."""
+    env = BackgammonEnv()
+    env.reset()
+
+    env.backgammon.board[:] = 0
+    env.backgammon.board[23] = -15
+    env.backgammon.off[0] = 15
+    assert env.backgammon.get_winner() == BACKGAMMON_STAKES
