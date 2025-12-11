2cbf89db96bd8ccbaa3a1b01422cfb4a55f66de3
Bartek Nowak <frexmaxxy@gmail.com>
2025-12-11 20:21:03 +0100

    init

---
+++ rlgammon/search/search_tree.py
@@ -0,0 +1,11 @@
+"""TODO."""
+
+
+class SearchTree:
+    """TODO."""
+
+    def __init__(self, max_depth: int, timeout: float) -> None:
+        """TODO."""
+
+    def search(self):
+        """TODO."""
