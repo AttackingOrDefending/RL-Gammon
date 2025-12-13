ed6bd53d58e2c6999458ade82d17c8546205e2ea
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2025-12-13 15:18:25 +0100

    move converter

--- converter.py
+++ converter.py
@@ -84,8 +84,10 @@ def flipped_to_normal_moves(move_str, player=1):
             i += 2  # Move to next pair
         return " ".join(moves)
 
-move = "P4-1-P19-1"
-player = 1
 
-print(flipped_to_normal_moves(move, player))
+if __name__ == "__main__":
+    move = "P4-1-P19-1"
+    player = 1
+
+    print(flipped_to_normal_moves(move, player))
 
--- icga/simple_converter.py
+++ icga/simple_converter.py
@@ -4,6 +4,7 @@ from rlgammon.agents.td_agent import TDAgent
 from rlgammon.rlgammon_types import WHITE
 
 from pyspiel_move import slash_to_px_distance
+from converter import flipped_to_normal_moves
 
 game = pyspiel.load_game("backgammon(scoring_type=full_scoring)")
 state = game.new_initial_state()
@@ -12,7 +13,7 @@ moves = [6, 1160, 15, 135, 595, 4, 986, 16, 324, 324, 3, 1109, 8, 1272, 0, 1168,
          4, 979, 14, 249, 3, 999, 6, 830, 6, 979, 0, 420, 17, 24, 442, 5, 596, 5, 518, 6, 1090, 18, 96, 81, 18, 648, 254,
          5, 697, 0, 37, 11, 882, 2, 239, 12, 730, 17, 224, 135, 9, 674, 14, 150, 4, 492, 19, 140, 410, 16, 1351, 7, 221, 9, 674,
          10]
-moves = [1]
+moves = [6, 1160, 15]
 think = True
 
 for m in moves:
@@ -38,6 +39,15 @@ else:
         action, evaluation = agent.choose_move(actions, state, return_eval=True)
         action_string = state.action_to_string(state.current_player(), action)
         discord = slash_to_px_distance(action_string.split(" - ")[1])
+        if state.current_player() == WHITE:
+            discord = discord.split('-')
+            ts = []
+            for t in discord:
+                if t.startswith('P'):
+                    ts.append('P'+str(25 - int(t[1:])))
+                else:
+                    ts.append(t)
+            discord = '-'.join(ts)
 
         print(f"-----------------------------------------------------------------")
         print("Player ", state.current_player(), " chosen action: ", action_string, "int: ", action, "    discord:", discord)
